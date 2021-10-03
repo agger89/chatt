95354bda8e42bb59baa9a3a99abb03682fe85bd1
조충은 <agger2154@gmail.com>
2021-10-03 15:16:02 +0900

    폴더 이동 및 webpack babel 세팅

---
+++ client/webpack.config.ts
@@ -0,0 +1,109 @@
+import path from 'path'
+// import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import webpack, { Configuration as WebpackConfiguration } from "webpack"
+// import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server"
+
+// interface Configuration extends WebpackConfiguration {
+//   devServer?: WebpackDevServerConfiguration
+// }
+
+// import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+const config = {
+  name: 'chatt',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      'hooks': path.resolve(__dirname, 'hooks'),
+      'components': path.resolve(__dirname, 'components'),
+      'layouts': path.resolve(__dirname, 'layouts'),
+      'pages': path.resolve(__dirname, 'pages'),
+      'utils': path.resolve(__dirname, 'utils'),
+      'typings': path.resolve(__dirname, 'typings'),
+    },
+  },
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: { browsers: ['IE 10'] },
+                debug: isDevelopment,
+              },
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript',
+          ],
+          env: {
+            development: {
+              plugins: [['@emotion', { sourceMap: true }]],
+            },
+            production: {
+              plugins: ['@emotion'],
+            },
+          },
+        },
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    // new ForkTsCheckerWebpackPlugin({
+    //   async: false,
+    //   // eslint: {
+    //   //   files: "./src/**/*",
+    //   // },
+    // }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3090,
+    devMiddleware: { publicPath: '/dist/' },
+    static: { directory: path.resolve(__dirname) },
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3095', // 주소를 3095로 변경해서 요청하겠다 (현재 client dev서버는 3090 // cors이슈)
+        changeOrigin: true,
+      },
+    },
+  },
+}
+
+// if (isDevelopment && config.plugins) {
+//   config.plugins.push(new webpack.HotModuleReplacementPlugin())
+//   config.plugins.push(new ReactRefreshWebpackPlugin({
+//     overlay: {
+//       useURLPolyfill: true
+//     }
+//   }))
+//   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server', openAnalyzer: true }))
+// }
+// if (!isDevelopment && config.plugins) {
+//   config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }))
+//   config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }))
+// }
+
+export default config
